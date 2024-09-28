cc362ee9f396e5eaf4511020ca0e6ddcbb13350a
Ivy Wang <120866069+inwang123@users.noreply.github.com>
2024-09-27 19:47:29 -0700

    Update comScript.js

--- comScript.js
+++ comScript.js
@@ -12,12 +12,12 @@
        modal.style.display = "block";
    }
 
-   // When the user clicks on <span> (x), close the modal
+   //clos modal when the user clicks on (x) 
    span.onclick = function() {
        modal.style.display = "none";
    }
 
-   // When the user clicks anywhere outside of the modal, close it
+   //close modal when user clicks outside of ti
    window.onclick = function(event) {
        if (event.target == modal) {
            modal.style.display = "none";
